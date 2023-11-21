b6a95b1e07bf372027b34b11402bf549d4d6fd51
deependra17 <deepen14kshetri@gmail.com>
2023-11-21 17:38:37 +0545

    git test

--- src/test/java/com/taf/PathwaysTest.java
+++ src/test/java/com/taf/PathwaysTest.java
@@ -3,6 +3,7 @@ package com.taf;
 public class PathwaysTest {
     public void pathwaysTest(){
         System.out.printf("This is a new pathways");
+        System.out.println("Again added file");
     }
 
 }
